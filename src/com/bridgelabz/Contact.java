732e4397aa6269c8f82fa7ccc6e083134bf4a53a
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-04-05 16:14:19 +0530

    Adding first name to Address book

---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+
+public class Contact {
+    private String firstName;
+    private String email;
+    private String phoneNumber;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
+
